14027964f9efcd65f4b966373776b0f6f9b92b80
am1553 <aryanmehta2016@gmail.com>
2021-10-31 17:24:09 +0000

    completed mobile version

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 // SELECTORS
 const quesBtn = document.getElementsByClassName("accordian-question");
 const answerOpen = document.getElementsByClassName("open");
-
 // const answerClose = document.getElementsByClassName("close");
 
 for(var i = 0; i < quesBtn.length; i++) {
+    const btnArrow = quesBtn[i].childNodes[1].children[1];
     const accordianAnswer = quesBtn[i].nextElementSibling;
     const questionHeading = document.getElementsByClassName("question-heading")[i];
     
     quesBtn[i].addEventListener("click", () => {
         // accordianAnswer.classList.toggle("close");
         
+        
         if(accordianAnswer.classList != "close") {
             accordianAnswer.classList.toggle("close");
-            questionHeading.classList.add("bold-text");
-        } else if (accordianAnswer.classList === "close"){
-  
-            questionHeading.classList.remove("bold-text");
-        } else {
-            questionHeading.classList.remove("bold-text");
+            questionHeading.classList.toggle("bold-text");
+            btnArrow.classList.toggle("btn-arrow");
         }
     })
     
